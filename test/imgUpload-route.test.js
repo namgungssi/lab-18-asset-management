5c7c414cad29e8291ddc31856b7bb0d245b25106
dalhun namgung <bnamgung@gmail.com>
2018-02-13 13:57:46 -0800

    fixing

---
+++ test/imgUpload-route.test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+
+
+const request = require('superagent');
+const expect = require('expect');
+const Image = require('../model/image');
+
+
+process.env.DB_URL = 'mongodb://localhost:27017/fileUpload_stg';
+process.env.AWS_BUCKET = 'namgungssi';
+const PORT = 4000;
+const HOST = 'http://localhost';
+const API = 'api/1.0';
+
+
+beforeAll(() => {
+  require('../lib/_server').start(PORT);
+  return Image.remove({});
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+  require('../lib/_server').stop;
+});
+
+
+describe('POST', () => {
+  let imgID = '';
+
+  test('creating a new image model returns an ID and a 200', () => {
+
+    return request
+      .post(`${HOST}:${PORT}/${API}/image`)
+      .send({ name:'me', path:'../model/files/8f532c2930ad87867d9f22c2ed2601.jpg' })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).not.toBe(undefined);
+        imgID = res.text;
+      });
+  });
+
+
+  test('creating a new image model returns an ID and a 200', () => {
+    return request
+      .post(`${HOST}:${PORT}/${API}/image/${imgID}/new-image`)
+      .field('title', 'me')
+      .set({'Content-Type':'multipart/form-data'})
+      .attach('photo', `${__dirname}/../model/files/8f532c2930ad87867d9f22c2ed2601.jpg`)
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).not.toBe(undefined);
+      });
+  });
+});
